f1b0a8ec04e0b8e83a219c7c45159b09110be5d8
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-08-26 11:43:21 -0700

    chore:init source

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace Maji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (args.First()) {
+                case "files":
+
+                    break;
+                case "strings":
+
+                    break;
+                default:
+
+                    break;
+            }
+        }
+    }
+}
