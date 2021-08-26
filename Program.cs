aeec7497bc2164ab9f55a6affaf6665f8cdbd402
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-08-26 12:52:06 -0700

    chore: add usage method

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ namespace Maji
     {
         static void Main(string[] args)
         {
+            if (args == null || args.Length == 0)
+            {
+                Usage();
+                return;
+            }
             switch (args.First()) {
                 case "files":
 
@@ -17,8 +22,26 @@ namespace Maji
                     break;
                 default:
 
+                default:
+                    Usage();
                     break;
             }
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("マージ (Māji) — Dirt-simple JSON merging.");
+            Console.WriteLine("Licensed under the 3-Clause BSD License.");
+            Console.WriteLine("Developed while jamming by Matt Ronchetto (doamatto)");
+            Console.WriteLine("Source: https://github.com/doamatto/maji");
+            Console.WriteLine("");
+            Console.WriteLine("=== === ===");
+            Console.WriteLine("");
+            Console.WriteLine("maji files <original> <patch file(s)> | You can specify as many patch files as your heart desires.");
+            Console.WriteLine("maji strings <original> <patch string(s)> | You can specify as many patch files as your heart desires.");
+            Console.WriteLine("");
+            return;
         }
     }
 }
