c915b6d9e4f88682290f30208caa1e8ab71b7ae6
Matt Ronchetto (doamatto) <hello@doamatto.xyz>
2021-08-26 13:21:27 -0700

    chore: gut string handling (see faq)

--- Program.cs
+++ Program.cs
@@ -10,84 +10,55 @@ namespace Maji
     {
         static void Main(string[] args)
         {
-            JObject jsonResp;
-            if (args == null || args.Length == 0)
+            // Handle no files with help information
+            if (args == null || args.Length == 0 || args.Length <1)
             {
                 Usage();
                 return;
             }
-            switch (args.First()) {
-                case "files":
-                    // Check if first file exists
-                    if (!File.Exists(args[1]))
-                    {
-                        throw new ArgumentException(String.Format(@"The file, {0}, does not exist.
-                        Please ensure the file is there, that Maji has permissions to read and
-                        write to that directory and/or file, and that no other issues (such as
-                        anti-virus) could be blocking it. Otherwise, submit an issue at
-                        https://github.com/doamatto/maji/issues", args[1]));
-                    }
-                    // Parse first file to variable
-                    string orig = File.ReadAllText(args[1]);
-                    jsonResp = JObject.Parse(orig);
-                    foreach (string file in args)
-                    {
-                        // Make sure we aren't using the command as a file somehow
-                        if (file == args.First())
-                        {
-                            continue;
-                        }
-                        // Make sure the file exists; if not throw an error
-                        if (!File.Exists(file))
-                        {
-                            throw new ArgumentException(String.Format(@"The file, {0}, does not exist.
-                            Please ensure the file is there, that Maji has permissions to read and
-                            write to that directory and/or file, and that no other issues (such as
-                            anti-virus) could be blocking it. Otherwise, submit an issue at
-                            https://github.com/doamatto/maji/issues", file));
-                        }
 
-                        // Read file into temp and merge
-                        string t = File.ReadAllText(file);
-                        jsonResp.Merge(JObject.Parse(t), new JsonMergeSettings
-                        {
-                            MergeArrayHandling = MergeArrayHandling.Union
-                        });
-                    }
-
-                    // Pass merged to file
-                    string name = "merged-" + Path.GetFileName(args[1]);
-                    string dir = Path.GetDirectoryName(args[1]);
-                    File.WriteAllText(String.Format("{dir}/merged-{0}", dir, name), jsonResp.ToString());
-
-                    break;
-
-                case "strings":
-                    // Parse first item
-                    jsonResp = JObject.Parse(args[1]);
-                    for (int i=0; i<args.Length;i++)
-                    {
-                        // Make sure we aren't using the command as a string somehow
-                        if (args[0] == args.First())
-                        {
-                            continue;
-                        }
-
-                        // Parse string into return
-                        jsonResp.Merge(JObject.Parse(args[i]), new JsonMergeSettings
-                        {
-                            MergeArrayHandling = MergeArrayHandling.Union
-                        });
-                    }
-
-                    // Return merged strings
-                    Console.WriteLine(jsonResp.ToString());
-                    break;
-
-                default:
-                    Usage();
-                    break;
+            // Check if first file exists
+            if (!File.Exists(args[1]))
+            {
+                throw new ArgumentException(String.Format(@"The file, {0}, does not exist.
+                Please ensure the file is there, that Maji has permissions to read and
+                write to that directory and/or file, and that no other issues (such as
+                anti-virus) could be blocking it. Otherwise, submit an issue at
+                https://github.com/doamatto/maji/issues", args[1]));
             }
+            // Parse first file to variable
+            string orig = File.ReadAllText(args[1]);
+            JObject jsonResp = JObject.Parse(orig);
+            foreach (string file in args)
+            {
+                // Make sure we aren't using the command as a file somehow
+                if (file == args.First())
+                {
+                    continue;
+                }
+                // Make sure the file exists; if not throw an error
+                if (!File.Exists(file))
+                {
+                    throw new ArgumentException(String.Format(@"The file, {0}, does not exist.
+                    Please ensure the file is there, that Maji has permissions to read and
+                    write to that directory and/or file, and that no other issues (such as
+                    anti-virus) could be blocking it. Otherwise, submit an issue at
+                    https://github.com/doamatto/maji/issues", file));
+                }
+
+                // Read file into temp and merge
+                string t = File.ReadAllText(file);
+                jsonResp.Merge(JObject.Parse(t), new JsonMergeSettings
+                {
+                    MergeArrayHandling = MergeArrayHandling.Replace
+                });
+            }
+
+            // Pass merged to file
+            string name = "merged-" + Path.GetFileName(args[1]);
+            string dir = Path.GetFullPath(args[1]);
+            string newpath = String.Format("{0}merged-{1}", dir, name);
+            File.WriteAllText(newpath, jsonResp.ToString());
         }
 
         static void Usage()
--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # マージ
-マージ (romanised: Māji, English translation: merge) is a dirt-simple tool to merge JSON strings and files.
+マージ (romanised: Māji, English translation: merge) is a dirt-simple tool to merge JSON files.
 
 ## Building from source
 1. Install [.NET Core](https://docs.microsoft.com/en-us/dotnet/core/install/) (developed using SDK 5.0.400)
 2. Build the project (`dotnet build -v d`) (see [here](https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet-build))
 
 ## Usage
-- `maji files`: Merge one or more files into the first file mentioned.
+- `maji`: Merge one or more files into the first file mentioned.
   - **Warning:** You should back-up both files prior to ensure no data is lost in the event that the merge isn't how you expected it to be.
   - Example: `maji files original.json patch1.json patch2.json`
-- `maji strings`: Merge one or more strings of JSON into the first string.
-  - Example: `maji strings {"1": "yay"} {"2": "yayuh"}`
-  - Response: `{"1": "yay", "2": "yayuh"}`
 
 ## FAQ
 **Q: Why did you make this?** Two reasons: needed to merge rules for [Ella](https://github.com/doamatto/ella-filters) and wanted to try out Newtonsoft after all these years. The last time I played with Visual Studio was 2013, and before that I used Visual Studio 2008 (I feel old).<br/>
-**Q: Is this Windows exclusive? Does it work in Wine? Why?** Technically: yes, this only works on Windows. I have seen mentions that installing .NET Framework 4.7.1 (see [here](https://www.reddit.com/r/wine_gaming/comments/8r6low)) can resolve issues with Newtonsoft in Wine, but I can't guarantee it. As far as I'm aware, there aren't any other well-crafted JSON libraries outside of Json.NET for C#.
-**Q: Will you create a native version for macOS, Linux, and BSD?** If it does not work at all on Linux, then I likely will write a v2 of マージ for that reason. What it would be written in is beyond me, as I want to keep experimenting with random tools and languages.
+**Q: Is this Windows exclusive? Does it work in Wine? Why?** Technically: yes, this only works on Windows. I have seen mentions that installing .NET Framework 4.7.1 (see [here](https://www.reddit.com/r/wine_gaming/comments/8r6low)) can resolve issues with Newtonsoft in Wine, but I can't guarantee it. As far as I'm aware, there aren't any other well-crafted JSON libraries outside of Json.NET for C#.<br/>
+**Q: Will you create a native version for macOS, Linux, and BSD?** If it does not work at all on Linux, then I likely will write a v2 of マージ for that reason. What it would be written in is beyond me, as I want to keep experimenting with random tools and languages.<br/>
+**Q: The git history shows you tried to add strings. Where are they now?** I quickly realised it wasn't practical. People would have to *manually* escape all the quotation marks in their JSON, which would be a lot to ask of anyone. For that reason, I kept only support for converting files.
